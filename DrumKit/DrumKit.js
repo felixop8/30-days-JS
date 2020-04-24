b2672cae07bd2f58f02caedbefd060c44faee98f
Felix Lopez <felix.lopez@vacasa.com>
2020-04-24 14:11:51 -0700

    Adds logic to remove element class on transition end.

--- DrumKit/DrumKit.js
+++ DrumKit/DrumKit.js
@@ -1,7 +1,22 @@
 window.addEventListener('keydown', (e) => {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    console.log(audio);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if(!audio) return; // if data-key is not defined stop the function from running all together.
+    key.classList.add('playing');
     audio.currentTime = 0; // Rewind to the start
     audio.play();
 });
+
+
+// Select all the elements with the class `.key`.
+// For each element add an event listener to trigger an anonymous function when the transisions of the element ends.
+// Since there are multiple transitions pick one, normally the one that takes more time, in this case all of the them
+// have the same transition time so I am going to randomly pick `transform`.
+// The reason to do this is because if you think about it, the anonymous function is going to be call multiple times
+// for each transition end, so we don't want to be constantly removing that class, just pick one of them and return 
+// the function for the rest.
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', (e) => {
+    if(!Object.is(e.propertyName, 'transform')) return;
+    key.classList.remove('playing');
+}));
---
+++ DrumKit/Notes.md
@@ -0,0 +1,12 @@
+## Drum Kit
+
+**Main Concepts**
+
+ 1. HTML data attributes: `data-key`
+ 2. HTML Audio element `<audio  data-key="65"  src="sounds/clap.wav"></audio>`
+ 3. `<kbd>` HTML tag: used to identify text that represents user keyboard input.
+ 4.  Audio/Video DOM Reference: Method - `play()` Property - `currentTime` 
+ 5. Query data attributes: `document.querySelector(audio[data-key="65"])`;
+ 6. JS Event Listeners: `keydown` and `transitionend`
+ 7. JS Array `forEach`
+
